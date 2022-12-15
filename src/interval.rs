475d6f58de9b05aa61111e47702cb62f5a130be9
Michael Adler <adlerm@mailbox.org>
2022-12-15 21:49:58 +0100

    merge intervals algorithm

--- src/interval.rs
+++ src/interval.rs
@@ -27,6 +27,40 @@ impl ClosedInterval {
     }
 }
 
+/// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping
+/// intervals, and return an array of the non-overlapping intervals that cover all the intervals in
+/// the input.
+///
+/// Example:
+///
+/// Input: `[[1,3],[2,6],[8,10],[15,18]]`
+/// Output: `[[1,6],[8,10],[15,18]]`
+pub fn merge_intervals(intervals: &[ClosedInterval]) -> Vec<ClosedInterval> {
+    // see https://leetcode.com/problems/merge-intervals/description/
+    // based on https://leetcode.com/problems/merge-intervals/solutions/21223/beat-98-java-sort-start-end-respectively/
+    let n = intervals.len();
+    let mut starts = Vec::with_capacity(n);
+    let mut ends = Vec::with_capacity(n);
+    for int in intervals {
+        starts.push(int.a);
+        ends.push(int.b);
+    }
+    starts.sort_unstable();
+    ends.sort_unstable();
+    let (starts, ends) = (starts, ends);
+    let mut res = Vec::with_capacity(n);
+    let mut i = 0;
+    let mut j = 0;
+    while i < n {
+        if i == n - 1 || unsafe { starts.get_unchecked(i + 1) > ends.get_unchecked(i) } {
+            res.push(ClosedInterval::new(starts[j], ends[i]));
+            j = i + 1;
+        }
+        i += 1;
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,5 +86,33 @@ mod tests {
         assert_eq!(false, y.disjoint(&x));
         assert_eq!(true, y.disjoint(&z));
         assert_eq!(true, z.disjoint(&y));
+    }
+
+    #[test]
+    fn test_merge_intervals() {
+        {
+            let intervals = vec![
+                ClosedInterval::new(1, 3),
+                ClosedInterval::new(2, 6),
+                ClosedInterval::new(8, 10),
+                ClosedInterval::new(15, 18),
+            ];
+            let result = merge_intervals(&intervals);
+            assert_eq!(
+                vec![
+                    ClosedInterval::new(1, 6),
+                    ClosedInterval::new(8, 10),
+                    ClosedInterval::new(15, 18)
+                ],
+                result,
+                "example 1"
+            );
+        }
+
+        {
+            let intervals = vec![ClosedInterval::new(1, 4), ClosedInterval::new(4, 5)];
+            let result = merge_intervals(&intervals);
+            assert_eq!(vec![ClosedInterval::new(1, 5),], result, "example 2");
+        }
     }
 }
