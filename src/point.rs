368640dd5b2172315e9987de31cb5fdb1314ba13
Michael Adler <adlerm@mailbox.org>
2023-01-16 21:45:10 +0100

    feat: generic Point2D

--- src/point.rs
+++ src/point.rs
@@ -1,34 +1,46 @@
-use std::fmt::Display;
-use std::ops;
+use core::fmt;
+use core::ops;
+
+use num_integer::Integer;
+use num_traits::Signed;
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone, PartialOrd, Ord, Hash, Default)]
-pub struct Point2D {
-    pub x: i64,
-    pub y: i64,
+pub struct Point2D<T = i64>
+where
+    T: Integer + Signed + Copy,
+{
+    pub x: T,
+    pub y: T,
 }
 
-impl Point2D {
-    pub fn new(x: i64, y: i64) -> Self {
+impl<T> Point2D<T>
+where
+    T: Integer + Signed + Copy,
+{
+    pub fn new(x: T, y: T) -> Self {
         Point2D { x, y }
     }
 
     /// Compute the Manhattan distance between `self` and `other`.
-    pub fn manhattan(&self, other: &Point2D) -> i64 {
+    pub fn manhattan(&self, other: Point2D<T>) -> T {
         (self.x - other.x).abs() + (self.y - other.y).abs()
     }
 
     /// Compute the Euclidean squared distance between `self` and `other`.
-    pub fn euclidean_squared(&self, other: &Point2D) -> i64 {
+    pub fn euclidean_squared(&self, other: Point2D<T>) -> T {
         let dx = self.x - other.x;
         let dy = self.y - other.y;
         dx * dx + dy * dy
     }
 }
 
-impl ops::Add<Point2D> for Point2D {
-    type Output = Point2D;
+impl<T> ops::Add<Point2D<T>> for Point2D<T>
+where
+    T: Integer + Signed + Copy,
+{
+    type Output = Point2D<T>;
 
-    fn add(self, rhs: Point2D) -> Self::Output {
+    fn add(self, rhs: Point2D<T>) -> Self::Output {
         Self {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
@@ -36,10 +48,13 @@ impl ops::Add<Point2D> for Point2D {
     }
 }
 
-impl ops::Sub<Point2D> for Point2D {
-    type Output = Point2D;
+impl<T> ops::Sub<Point2D<T>> for Point2D<T>
+where
+    T: Integer + Signed + Copy,
+{
+    type Output = Point2D<T>;
 
-    fn sub(self, rhs: Point2D) -> Self::Output {
+    fn sub(self, rhs: Point2D<T>) -> Self::Output {
         Self {
             x: self.x - rhs.x,
             y: self.y - rhs.y,
@@ -47,7 +62,10 @@ impl ops::Sub<Point2D> for Point2D {
     }
 }
 
-impl Display for Point2D {
+impl<T> fmt::Display for Point2D<T>
+where
+    T: Integer + Signed + Copy + fmt::Display,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
@@ -103,7 +121,7 @@ impl ops::Sub<Point3D> for Point3D {
     }
 }
 
-impl Display for Point3D {
+impl fmt::Display for Point3D {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {}, {})", self.x, self.y, self.z)
     }
@@ -116,12 +134,12 @@ mod tests {
     #[test]
     fn test_manhattan() {
         let p = Point2D::new(0, 0);
-        assert_eq!(30, p.manhattan(&Point2D { x: 10, y: 20 }));
+        assert_eq!(30, p.manhattan(Point2D { x: 10, y: 20 }));
     }
 
     #[test]
     fn test_euclidean_squared() {
         let p = Point2D::new(0, 0);
-        assert_eq!(500, p.euclidean_squared(&Point2D { x: 10, y: 20 }));
+        assert_eq!(500, p.euclidean_squared(Point2D { x: 10, y: 20 }));
     }
 }
